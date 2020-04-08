1ab8bebf898d1d7d7e3742c282de8dcb757c92da
Tong Dang Tinh <tinhtongdang@gmail.com>
2020-04-08 16:41:21 +0700

    Add friendly gem, will_paginate gem, and some things

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,23 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show] 
+  before_action :is_admin!, except: [:index, :show]
+  before_action :find_post, only: [:show]
 
+  def index
+    @posts = Post.all.order("created_at DESC").paginate(page: params[:page])
+  end
+  
+  def show
+    views = @post.views + 1
+    @post.update(views: views)
+  end
   def new
     @post = Post.new
   end
 
   def create
     @post = Post.new post_params
+    @post.user = current_user
     if @post.save 
       flash[:success]  = "Post was successfully created."
       redirect_to posts_path
@@ -15,6 +27,10 @@ class PostsController < ApplicationController
   end
 
   private
+
+    def find_post
+      @post = Post.friendly.find params[:id]
+    end
 
     def post_params
       params.require(:post).permit :title, :thumnail, :banner, :body
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :thumbnail, styles: { medium: "300x300>", 
-    thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  has_attached_file :banner, styles: { medium: "300x300>", thumb: "100x100>" }, 
+  has_attached_file :banner, styles: { medium: "300x100>", thumb: "100x100>" }, 
     default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\z/
   validates_attachment_content_type :banner, content_type: /\Aimage\/.*\z/
 
   validates :title, presence: true, length: { minimum:  5 } 
   validates :body, presence: true, length: { minimum: 25 }
+
+  self.per_page = 10
+
+  extend FriendlyId
+  friendly_id :title, use: :slugged
 end
